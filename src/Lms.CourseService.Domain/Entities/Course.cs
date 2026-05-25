7e1d551d34c5163a77f5862787c2b5bf0a7c774b
JoachimPersson88 <joachim.persson88@gmail.com>
2026-05-25 22:58:44 +0200

    feat: add Course entity and configure EF Core DbContext

---
+++ src/Lms.CourseService.Domain/Entities/Course.cs
@@ -0,0 +1,18 @@
+﻿namespace Lms.CourseService.Domain.Entities;
+
+public class Course
+{
+    public int Id { get; set; }
+
+    public string Title { get; set; } = null!;
+
+    public string Description { get; set; } = null!;
+
+    public string ImageUrl { get; set; } = null!;
+
+    public string Instructor { get; set; } = null!;
+
+    public int LessonsCount { get; set; }
+
+    public string Duration { get; set; } = null!;
+}
---
+++ src/Lms.CourseService.Infrastructure/Data/CourseDbContext.cs
@@ -0,0 +1,10 @@
+﻿using Lms.CourseService.Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace Lms.CourseService.Infrastructure.Data;
+
+public class CourseDbContext(DbContextOptions<CourseDbContext> options)
+    : DbContext(options)
+{
+    public DbSet<Course> Courses => Set<Course>();
+}
