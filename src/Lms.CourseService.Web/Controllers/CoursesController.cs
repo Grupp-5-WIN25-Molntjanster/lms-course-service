7a3e64b8284be68dac4e5ee82b3d87e45040c559
JoachimPersson88 <joachim.persson88@gmail.com>
2026-05-25 23:02:56 +0200

    feat: add CoursesController with GET endpoint

---
+++ src/Lms.CourseService.Web/Controllers/CoursesController.cs
@@ -0,0 +1,18 @@
+﻿using Lms.CourseService.Infrastructure.Data;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace Lms.CourseService.Web.Controllers;
+
+[ApiController]
+[Route("api/courses")]
+public class CoursesController(CourseDbContext context) : ControllerBase
+{
+    [HttpGet]
+    public async Task<IActionResult> GetCourses()
+    {
+        var courses = await context.Courses.ToListAsync();
+
+        return Ok(courses);
+    }
+}
