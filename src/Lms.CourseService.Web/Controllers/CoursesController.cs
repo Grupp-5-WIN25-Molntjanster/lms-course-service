cba8049581c5c2e43db2aec3a895831aa7506b1b
JoachimPersson88 <joachim.persson88@gmail.com>
2026-05-26 15:20:10 +0200

    feat: add get course by id endpoint

--- src/Lms.CourseService.Web/Controllers/CoursesController.cs
+++ src/Lms.CourseService.Web/Controllers/CoursesController.cs
@@ -1,4 +1,5 @@
-﻿using Lms.CourseService.Infrastructure.Data;
+﻿using Lms.CourseService.Domain.Entities;
+using Lms.CourseService.Infrastructure.Data;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 
@@ -14,5 +15,17 @@ public class CoursesController(CourseDbContext context) : ControllerBase
         var courses = await context.Courses.ToListAsync();
 
         return Ok(courses);
+    }
+
+    [HttpGet("{id:int}")]
+    public async Task<IActionResult> GetCourseById(int id)
+    {
+        var course = await context.Courses
+            .FirstOrDefaultAsync(x => x.Id == id);
+
+        if (course is null)
+            return NotFound();
+
+        return Ok(course);
     }
 }
