bcf3007ccdd86649aba2b6836a9a3e0ed7a83484
JoachimPersson88 <joachim.persson88@gmail.com>
2026-05-26 15:24:22 +0200

    feat: return course dto from endpoints

--- src/Lms.CourseService.Web/Controllers/CoursesController.cs
+++ src/Lms.CourseService.Web/Controllers/CoursesController.cs
@@ -2,6 +2,7 @@
 using Lms.CourseService.Infrastructure.Data;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
+using Lms.CourseService.Application.DTOs;
 
 namespace Lms.CourseService.Web.Controllers;
 
@@ -12,7 +13,18 @@ public class CoursesController(CourseDbContext context) : ControllerBase
     [HttpGet]
     public async Task<IActionResult> GetCourses()
     {
-        var courses = await context.Courses.ToListAsync();
+        var courses = await context.Courses
+            .Select(x => new CourseDto
+            {
+                Id = x.Id,
+                Title = x.Title,
+                Description = x.Description,
+                ImageUrl = x.ImageUrl,
+                Instructor = x.Instructor,
+                LessonsCount = x.LessonsCount,
+                Duration = x.Duration
+            })
+            .ToListAsync();
 
         return Ok(courses);
     }
@@ -21,7 +33,18 @@ public class CoursesController(CourseDbContext context) : ControllerBase
     public async Task<IActionResult> GetCourseById(int id)
     {
         var course = await context.Courses
-            .FirstOrDefaultAsync(x => x.Id == id);
+            .Where(x => x.Id == id)
+            .Select(x => new CourseDto
+            {
+                Id = x.Id,
+                Title = x.Title,
+                Description = x.Description,
+                ImageUrl = x.ImageUrl,
+                Instructor = x.Instructor,
+                LessonsCount = x.LessonsCount,
+                Duration = x.Duration
+            })
+            .FirstOrDefaultAsync();
 
         if (course is null)
             return NotFound();
